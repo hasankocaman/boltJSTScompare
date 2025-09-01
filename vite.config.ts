39dc51cf7d65cd072857835ca7c358f0167f4f25
hasankocaman <94443851+hasankocaman@users.noreply.github.com>
2025-09-01 11:19:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/boltJSTScompare/',
   plugins: [react()],
   optimizeDeps: { exclude: ['lucide-react'] },
 })
